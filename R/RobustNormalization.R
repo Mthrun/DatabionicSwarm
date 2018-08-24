830be28777a81252caded258dd894e49633933cc
Mthrun <m.thrun@gmx.net>
2018-08-24 14:57:40 +0200

    .

--- R/RobustNormalization.R
+++ R/RobustNormalization.R
@@ -1,5 +1,9 @@
-RobustNormalization = function (Data,Centered=FALSE,Capped=FALSE,na.rm=TRUE) 
+RobustNormalization = function (Data,Centered=FALSE,Capped=FALSE,na.rm=TRUE,WithBackTransformation=FALSE) 
 {
+  center=NULL
+  Denom=NULL
+  minX=NULL
+  maxX=NULL
   if (is.vector(Data)) {
     quants = quantile(Data, c(0.01, 0.5, 0.99),na.rm = na.rm)
     minX = quants[1]
@@ -9,7 +13,8 @@ RobustNormalization = function (Data,Centered=FALSE,Capped=FALSE,na.rm=TRUE)
     Data=(Data - minX)/Denom
     #median(abs(Data-median(Data,na.rm=T)),na.rm=T)
     if(Centered){
-      Data=Data-median(Data,na.rm=na.rm)
+      center=median(Data,na.rm=na.rm)
+      Data=Data-center
       if(Capped){
         maxX=1
         minX=-1
@@ -25,15 +30,39 @@ RobustNormalization = function (Data,Centered=FALSE,Capped=FALSE,na.rm=TRUE)
         Data[Data<minX]=minX
       }
     }
-    return(Data)
+    if(WithBackTransformation)
+      return(list(TransformedData=Data,MinX=minX,MaxX=maxX,Denom=Denom,Center=center))
+    else
+      return(Data)
   }
   else if (is.matrix(Data)) {
+    if(WithBackTransformation){
+      cols = ncol(Data)
+      xtrans = list()
+      DataOut=c()
+      minX=c()
+      maxX=c()
+      Denom=c()
+      center=c()
+      for (i in 1:cols) {
+        xtrans = RobustNormalization(Data = as.vector(Data[, i]),Centered = Centered,Capped = Capped,na.rm = na.rm,WithBackTransformation=WithBackTransformation)
+        DataOut=cbind(DataOut,as.matrix(xtrans$TransformedData))
+        minX=cbind(minX,xtrans$MinX)
+        maxX=cbind(maxX,xtrans$MaxX)
+        Denom=cbind(Denom,xtrans$Denom)
+        center=cbind(center,xtrans$Center)
+      }
+      
+      return(list(TransformedData=DataOut,MinX=minX,MaxX=maxX,Denom=Denom,Center=center))
+    }else{
     cols = ncol(Data)
     xtrans = Data
     for (i in 1:cols) {
-      xtrans[, i] = RobustNormalization(as.vector(Data[, i]),Centered,Capped,na.rm)
+      xtrans[, i] = RobustNormalization(as.vector(Data[, i]),Centered,Capped,na.rm,WithBackTransformation=WithBackTransformation)
     }
-    return(xtrans)
+
+      return(xtrans)
+    }
   }
   else {
     tryCatch({
