442e97529e88b51ced34360722a23ede36a21e3e
Mthrun <m.thrun@gmx.net>
2023-10-30 13:31:37 +0100

    bugfix RelativeDifference, Optional parameter

--- R/RelativeDifference.R
+++ R/RelativeDifference.R
@@ -5,13 +5,14 @@ RelativeDifference=function(X,Y,epsilon=10^-10,na.rm=FALSE,Silent=FALSE){
     Y <- Y[noNaNInd]
   }
   if(length(X)!=length(Y)) stop('Length of X and Y do not match.')
-  if(length(X)>1) return(mapply(X,FUN = RelativeDifference,Y))
+  if(length(X)>1) return(mapply(X,FUN = RelativeDifference,Y,epsilon=epsilon,na.rm=na.rm,Silent=Silent))
   oben=Y-X
   unten=X+Y
   if(!is.finite(unten)) stop('Some of your values are not finite. Please use na.rm=TRUE')
   if(abs(unten)<epsilon){
-    if(isFALSE(Silent))
+    if(isFALSE(Silent)){
       message('RelativeDifference: X + Y are smaller than epsilon, returning 0')
+    }    
     
     return(0)
   }
